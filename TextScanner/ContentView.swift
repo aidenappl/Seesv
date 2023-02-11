fce4f8c4ad66cce3c8478e6511609e23e3a4b3f9
Aiden Appleby <aiden.appleby3@gmail.com>
2023-02-11 13:07:40 -0500

    added searching a csv

--- TextScanner/ContentView.swift
+++ TextScanner/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
     
     @EnvironmentObject var vm: AppViewModel
     
+    var csvString = "Serial Number,Device Name,Model,Checked\nDMPZTE6RKD6L,Aiden’s iPad,A1980,FALSE\nMZX6DQAZ8AYF,iPad,A1980,FALSE\nXZMXUEXYKER9,iPad,A1980,FALSE\nJLPRH2P2ESQ7,iPad,A1980,FALSE\nJB6VPKMB2V47,iPad,A1980,FALSE\nHLX9YQDT7NYF,iPad,A1980,FALSE\nZLC5U2SZXUQU,iPad,A1980,FALSE\nHPC3HASX47FD,iPad,A1980,FALSE\n4PJFLLLN5CCL,iPad,A1980,FALSE\nE8KCLZM9Q4L9,iPad,A1980,FALSE\nERV2J8T6KW2L,iPad,A1980,FALSE\n4NTPRUC8DAJF,iPad,A1980,FALSE\nSBTTP2GMCSD7,iPad,A1980,FALSE\nUYY338C99FUC,iPad,A1980,FALSE\nF5289HVQ33BX,iPad,A1980,FALSE\n5UW6M4ZJCJGG,iPad,A1980,FALSE\n9GV27AWH3JTR,iPad,A1980,FALSE\nPDVVN9YHVSE6,iPad,A1980,FALSE\nWNC5TCMXR8TB,iPad,A1980,FALSE\nZM7MJEKC9BCJ,iPad,A1980,FALSE\nGEGHC87NJRR2,iPad,A1980,FALSE\n8YLUB7TXX74D,iPad,A1980,FALSE\nY4LE5BTYYQMF,iPad,A1980,FALSE\nH2BKWBTCLPLL,iPad,A1980,FALSE\n3D5U6KKWMQXA,iPad,A1980,FALSE\nTFTD43SVNM2E,iPad,A1980,FALSE\nATJRLU2KSN59,iPad,A1980,FALSE\n8SWJUBECJAHB,iPad,A1980,FALSE\nSVVLL5HYH2BL,iPad,A1980,FALSE\n7RQ96Q5667YY,iPad,A1980,FALSE\nL934WPUZTLQG,iPad,A1980,FALSE\n2VXRADKM32TN,iPad,A1980,FALSE\nHH8B2Z5FJDLN,iPad,A1980,FALSE\nXQ4G9XGRB9SF,iPad,A1980,FALSE\n6GE66KKJTJW5,iPad,A1980,FALSE\n4NDAYHPDLKYZ,iPad,A1980,FALSE\nX7K4A9NMVCGM,iPad,A1980,FALSE\n7P2VEEYCPK4X,iPad,A1980,FALSE\nQVT35HKPJ43H,iPad,A1980,FALSE\nNBM89CQTAU45,iPad,A1980,FALSE\nTWDWRC86CRY2,iPad,A1980,FALSE\n7MWAQMBQVELG,iPad,A1980,FALSE\nMV23W7ASDNCT,iPad,A1980,FALSE\nAW5XLPAJJSAH,iPad,A1980,FALSE\nL83MT9JALMFT,iPad,A1980,FALSE\nH5FEYYM2W83M,iPad,A1980,FALSE\n4X8DTAEEF53S,iPad,A1980,FALSE\nG297BXKCTHN6,iPad,A1980,FALSE\n3YET73K6HG8S,iPad,A1980,FALSE\n87TS5JGUQWFN,iPad,A1980,FALSE\n6F76CKHPC5SP,iPad,A1980,FALSE\nW8US5W59SB9U,iPad,A1980,FALSE\nYCYG658TMTVW,iPad,A1980,FALSE\nJFGKXN6UQ835,iPad,A1980,FALSE\n3R3PLP4LFQLZ,iPad,A1980,FALSE\nYEZUFPJKL5PE,iPad,A1980,FALSE\nT4FSJ7KM6RSV,iPad,A1980,FALSE\n6U4HRM9V4FSA,iPad,A1980,FALSE\n9RB456YSWY42,iPad,A1980,FALSE\nPEN4S72ULF2M,iPad,A1980,FALSE\nJCR7TSSP2A3B,iPad,A1980,FALSE\n27EZK8MK3BKL,iPad,A1980,FALSE\nVN5MMRB7PDT8,iPad,A1980,FALSE\nUTW2QV75JQAC,iPad,A1980,FALSE\nB8HAWSUWQVFZ,iPad,A1980,FALSE\n8RHG2LNLGKQF,iPad,A1980,FALSE\nR66LAZG5Q9YB,iPad,A1980,FALSE\n5T28LAY7XVEW,iPad,A1980,FALSE\nZRRPC8WURR28,iPad,A1980,FALSE\n2VELJJ6GMVA7,iPad,A1980,FALSE\n7TTRAYSHWL5A,iPad,A1980,FALSE\nJAAA3ZLSWY86,iPad,A1980,FALSE\n43F6DYHV7MES,iPad,A1980,FALSE\nY7ECTN5QKXT3,iPad,A1980,FALSE\nVPSFMN88JQUB,iPad,A1980,FALSE\nE26E48BBPYKW,iPad,A1980,FALSE\nTT5M7L24R33R,iPad,A1980,FALSE\nVNGSVGNJ5JAH,iPad,A1980,FALSE\n6YX7ZRD4YQVX,iPad,A1980,FALSE\nKDYYAHUFPUZV,iPad,A1980,FALSE\nGLLMCQ524EJE,iPad,A1980,FALSE\nF7533XWAUS6J,iPad,A1980,FALSE\n66K42772NP6S,iPad,A1980,FALSE\n8R9FYQ2QALQN,iPad,A1980,FALSE\n4NXCYDAR4PF3,iPad,A1980,FALSE\nT8WJZYAR95PK,iPad,A1980,FALSE\nKSEYDJJ5C6QK,iPad,A1980,FALSE\nMNWPL4GA5YF8,iPad,A1980,FALSE\nKNBUJYXRYVA5,iPad,A1980,FALSE\nC2TSGR6A6NCB,iPad,A1980,FALSE\nZRD3WPZMD8NP,iPad,A1980,FALSE\nDQKGSZNY72N3,iPad,A1980,FALSE\nW8S4YJZ88XXQ,iPad,A1980,FALSE\nM9R7JCSAA84E,iPad,A1980,FALSE\n7Z25DJHW5LHM,iPad,A1980,FALSE\nRXS6BMNDRJV7,iPad,A1980,FALSE\nRJBJSMDF4LCE,iPad,A1980,FALSE\n3WMWCLVY5JT3,iPad,A1980,FALSE\nE6N6BMJ4EP9B,iPad,A1980,FALSE\nZ3HRS992RJCL,iPad,A1980,FALSE"
 
     private let textContentTypes: [(title: String, textContentType: DataScannerViewController.TextContentType?)] = [
         ("All", .none),
@@ -59,21 +60,39 @@ struct ContentView: View {
                                 let model = processModel(text: text.transcript)
                                 let serial = processSerial(text: text.transcript)
                                 if serial != "" {
+                                    let deviceFound = searchCSV(csv: csvString, columnHeader: "Serial Number", searchValue: serial)
+                                    
                                     if (model != "") {
                                         Text("Model: \(model) \nSerial: \(serial)")
                                     } else {
                                         Text("Serial: \(serial)")
                                     }
-                                    Button(action: {
-                                    }) {
-                                        Text("View Device")
-                                            .foregroundColor(.white)
-                                            .frame(minWidth: 0, maxWidth: .infinity)
-                                            .padding()
-                                            .background(Color.blue)
-                                            .cornerRadius(10)
+                                    
+                                    if deviceFound.count > 0 {
+                                        Button(action: {
+                                            print(deviceFound)
+                                        }) {
+                                            Text("View Device")
+                                                .foregroundColor(.white)
+                                                .frame(minWidth: 0, maxWidth: .infinity)
+                                                .padding()
+                                                .background(Color.blue)
+                                                .cornerRadius(10)
+                                        }
+                                        .frame(minWidth: 0, maxWidth: .infinity)
+                                    } else {
+                                        Button(action: {
+//                                            Do Something
+                                        }) {
+                                            Text("No Devices Found...")
+                                                .foregroundColor(.white)
+                                                .frame(minWidth: 0, maxWidth: .infinity)
+                                                .padding()
+                                                .background(Color.gray)
+                                                .cornerRadius(10)
+                                        }
+                                        .frame(minWidth: 0, maxWidth: .infinity)
                                     }
-                                    .frame(minWidth: 0, maxWidth: .infinity)
                                 }
                             @unknown default:
                                 Text("Unknown")
@@ -117,6 +136,25 @@ struct ContentView: View {
         return ""
     }
     
+    func searchCSV(csv: String, columnHeader: String, searchValue: String) -> [String] {
+        let rows = csv.split(separator: "\n")
+        let header = rows.first!.split(separator: ",")
+        
+        // Find the index of the specified column header
+        guard let columnIndex = header.firstIndex(where: { String($0) == columnHeader }) else {
+            return []
+        }
+        
+        // Filter the rows to only include those where the specified column matches the search value
+        let filteredRows = rows.filter { row in
+            let columns = row.split(separator: ",")
+            return columns[columnIndex] == searchValue
+        }
+        
+        return filteredRows.map { String($0) }
+    }
+
+    
     private var headerView: some View {
         VStack {
             HStack {
@@ -138,5 +176,11 @@ struct ContentView: View {
             
             Text(vm.headerText).padding(.top)
         }.padding(.horizontal)
+    }
+    
+    struct DetailView: View {
+        var body: some View {
+            Text("This is the detail view")
+        }
     }
 }
