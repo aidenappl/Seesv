77a1bd1d2a0ee4abcaa6fa42b0315952458ecfc5
Aiden Appleby <aiden.appleby3@gmail.com>
2023-02-10 23:55:03 -0500

    Initial Commit

---
+++ TextScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TextScanner
+//
+//  Created by Aiden Appleby on 2/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextScanner/TextScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextScannerApp.swift
+//  TextScanner
+//
+//  Created by Aiden Appleby on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TextScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
